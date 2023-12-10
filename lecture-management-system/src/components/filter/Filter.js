bac2d17bfb71b86b29a0c03470182a53deb2eb11
mishrarpita321 <45449505+mishrarpita321@users.noreply.github.com>
2023-12-10 19:38:14 +0100

    calendar and filter changes

--- lecture-management-system/src/components/filter/Filter.js
+++ lecture-management-system/src/components/filter/Filter.js
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import "./style.css";
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import Dropdown from "../utils/Dropdown";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { Button } from "@mui/material";
 
 export const Filter = () => {
     const [isChecked, setIsChecked] = useState(false);
@@ -21,10 +25,45 @@ export const Filter = () => {
                 {options.map((value, index) => (
                     <div className="optionWrapper" onClick={handleCheckboxToggle}>
                         {isChecked ? <CheckBoxIcon /> : <CheckBoxOutlineBlankIcon />}
-                        <span>{value}</span>
+                        <span className="value">{value}</span>
                     </div>
                 ))}
-                <Dropdown />
+
+                <div className="batch">
+                    <p className="name">Batch</p>
+                    <FormControl sx={{ m: 1, minWidth: 120 }} size="small" >
+                        <Select style={{width: '200px'}}>
+                            <MenuItem value={9}>April 2022-2024</MenuItem>
+                            <MenuItem >October 2022-2024</MenuItem>
+                            <MenuItem >April 2023-2025</MenuItem>
+                            <MenuItem >October 2023-2025</MenuItem>
+                        </Select>
+                    </FormControl >
+                </div>
+                <div className="semester">
+                    <p>Semester</p>
+                    <FormControl sx={{ m: 1, minWidth: 120 }} size="small" >
+                        <Select style={{width: '200px'}}>
+                            <MenuItem value={9}>1</MenuItem>
+                            <MenuItem >2</MenuItem>
+                            <MenuItem >3</MenuItem>
+                            <MenuItem >4</MenuItem>
+                        </Select>
+                    </FormControl >
+                </div>
+                <div className="block">
+                    <p className="name">Block</p>
+                    <FormControl sx={{ m: 1, minWidth: 120 }} size="small" >
+                        <Select style={{width: '200px'}}>
+                            <MenuItem value={9}>09:00</MenuItem>
+                            <MenuItem >11:00</MenuItem>
+                            <MenuItem >02:00</MenuItem>
+                            <MenuItem >05:30</MenuItem>
+                        </Select>
+                    </FormControl >
+                </div>
+                {/* <button className="button">Submit</button> */}
+                <Button style={{backgroundColor: '#8D57D4', width: '100px', color: 'white'}}>Submit</Button>
             </div>
         </div>
 
