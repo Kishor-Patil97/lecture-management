f845f14d2e45b5d51ce798bf8c018c7ad568a8aa
mishrarpita321 <45449505+mishrarpita321@users.noreply.github.com>
2023-12-13 12:42:51 +0100

    logic for closing and showing Popup

--- lecture-management-system/src/components/popup/Popup.js
+++ lecture-management-system/src/components/popup/Popup.js
@@ -26,86 +26,96 @@ export const Popup = () => {
     const handleBlockChange = (event) => {
         setBlock(event.target.value);
     }
+
+    const [showPopup, setShowPopup] = useState(true);
+
+    const handlePopup = () => {
+        setShowPopup(false);
+    }
     const batchItems = ['April 2022-2024', 'October 2022-2024', 'April 2023-2025', 'October 2023-2025'];
     const courseItems = ['Applied Computer Science', 'Applied Data Science'];
     const semItems = ['1', '2', '3', '4'];
     const blockItems = ['1', '2', '3', '4', '5', '6', '7', '8'];
-    const moduleItems = ['International Project Management', 'Software Development Practice', 'IT-Security', 'UX Design and Implementation','Funfamentals towards UX'];
+    const moduleItems = ['International Project Management', 'Software Development Practice', 'IT-Security', 'UX Design and Implementation', 'Funfamentals towards UX'];
 
     return (
-        <div className={styles.form}>
-            <Grid container className={styles.container}>
-                <Grid item sm={6} className={styles.row}>
-                    <div>Batch:</div>
-                    <FormControl sx={{ m: 1 }} size="small" className={styles.formLM} >
-                        <Select value={batch} onChange={handleBatchChange}>
-                            {batchItems.map((option) => (
-                                <MenuItem key={option} value={option}>{option}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl >
+        <>
+        {showPopup && (
+            <div className={styles.form}>
+                <Grid container className={styles.container}>
+                    <Grid item sm={6} className={styles.row}>
+                        <div>Batch:</div>
+                        <FormControl sx={{ m: 1 }} size="small" className={styles.formLM} >
+                            <Select value={batch} onChange={handleBatchChange}>
+                                {batchItems.map((option) => (
+                                    <MenuItem key={option} value={option}>{option}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl >
+                    </Grid>
+                    <Grid item sm={6} className={styles.row}>
+                        <div>Study Program:</div>
+                        <FormControl sx={{ m: 1 }} size="small" className={styles.formLM}>
+                            <Select value={course} onChange={handleCourseChange}>
+                                {courseItems.map((option) => (
+                                    <MenuItem key={option} value={option}>{option}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl >
+                    </Grid>
+                    <Grid item sm={3} className={styles.row}>
+                        <div>Block:</div>
+                        <FormControl sx={{ m: 1, minWidth: 120 }} size="small" className={styles.margin} >
+                            <Select value={block} onChange={handleBlockChange}>
+                                {blockItems.map((option) => (
+                                    <MenuItem key={option} value={option}>{option}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl >
+                    </Grid>
+                    <Grid item sm={3} >
+                        <div>Semester:</div>
+                        <FormControl sx={{ m: 1, minWidth: 120 }} size="small" className={styles.margin}>
+                            <Select value={sem} onChange={handleSemChange}>
+                                {semItems.map((option) => (
+                                    <MenuItem key={option} value={option}>{option}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl >
+                    </Grid>
+                    <Grid item sm={6}>
+                        <div>Module:</div>
+                        <FormControl sx={{ m: 1 }} size="small" className={styles.formLM}>
+                            <Select value={sem} onChange={handleSemChange}>
+                                {moduleItems.map((option) => (
+                                    <MenuItem key={option} value={option}>{option}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl >
+                    </Grid>
+                    <Grid item sm={12} className={styles.addProf}>
+                        <div>Add Professors:</div>
+                        <MultiSelect />
+                    </Grid>
+                    <Grid item sm={6} className={styles.date}>
+                        <div>Start Date:</div>
+                        <input type="date" />
+                    </Grid>
+                    <Grid item sm={6} className={styles.date}>
+                        <div>End Date:</div>
+                        <input type="date" />
+                    </Grid>
+                    <Grid item>
+                        <div>Message for Professors:</div>
+                        <textarea rows='4' cols='76' className={styles.textarea}></textarea>
+                    </Grid>
+                    <Grid item sm={12} className={styles.buttondiv}>
+                        <Button className={styles.cancelBtn} onClick={handlePopup}>Cancel</Button>
+                        <Button className={styles.submitBtn} onClick={handlePopup}>Create</Button>
+                    </Grid>
                 </Grid>
-                <Grid item sm={6} className={styles.row}>
-                    <div>Study Program:</div>
-                    <FormControl sx={{ m: 1 }} size="small" className={styles.formLM}>
-                        <Select value={course} onChange={handleCourseChange}>
-                            {courseItems.map((option) => (
-                                <MenuItem key={option} value={option}>{option}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl >
-                </Grid>
-                <Grid item sm={3} className={styles.row}>
-                    <div>Block:</div>
-                    <FormControl sx={{ m: 1, minWidth: 120 }} size="small" className={styles.margin} >
-                        <Select value={block} onChange={handleBlockChange}>
-                            {blockItems.map((option) => (
-                                <MenuItem key={option} value={option}>{option}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl >
-                </Grid>
-                <Grid item sm={3} >
-                    <div>Semester:</div>
-                    <FormControl sx={{ m: 1, minWidth: 120 }} size="small" className={styles.margin}>
-                        <Select value={sem} onChange={handleSemChange}>
-                            {semItems.map((option) => (
-                                <MenuItem key={option} value={option}>{option}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl >
-                </Grid>
-                <Grid item sm={6}>
-                    <div>Module:</div>
-                    <FormControl sx={{ m: 1 }} size="small" className={styles.formLM}>
-                        <Select value={sem} onChange={handleSemChange}>
-                            {moduleItems.map((option) => (
-                                <MenuItem key={option} value={option}>{option}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl >
-                </Grid>
-                <Grid item sm={12} className={styles.addProf}>
-                    <div>Add Professors:</div>
-                    <MultiSelect />
-                </Grid>
-                <Grid item sm={6} className={styles.date}>
-                    <div>Start Date:</div>
-                    <input type="date" />
-                </Grid>
-                <Grid item sm={6} className={styles.date}>
-                    <div>End Date:</div>
-                    <input type="date" />
-                </Grid>
-                <Grid item>
-                    <div>Message for Professors:</div>
-                    <textarea rows='4' cols='76' className={styles.textarea}></textarea>
-                </Grid>
-                <Grid item sm={12} className={styles.buttondiv}>
-                    <Button className={styles.cancelBtn}>Cancel</Button>
-                    <Button className={styles.submitBtn}>Create</Button>
-                </Grid>
-            </Grid>
-        </div>
+            </div>
+        )}
+        </>
     );
 };
--- lecture-management-system/src/components/revertreasonpopup/Revertpopup.js
+++ lecture-management-system/src/components/revertreasonpopup/Revertpopup.js
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import styles from './Revertpopup.module.css';
 import { Button } from '@mui/material';
 
 export const Revertpopup = () => {
+    const [showPopup, setShowPopup] = useState(true);
+
+    const handlePopup = () => {
+        setShowPopup(false);
+    }
     return (
-        <div class={styles.divrevert}>
-            <div className={styles.group}>
-                <div className={styles.text}>Reason to revert:</div>
-                <textarea rows='4' cols='76' className={styles.textarea} ></textarea>
-            </div>
-            <div className={styles.btns}>
-                <Button className={styles.buttonCancel}>Cancel</Button>
-                <Button className={styles.buttonRevert}>Revert</Button>
-            </div>
-        </div>
+        <>
+            {showPopup && (
+                <div class={styles.divrevert}>
+                    <div className={styles.group}>
+                        <div className={styles.text}>Reason to revert:</div>
+                        <textarea rows='4' cols='76' className={styles.textarea} ></textarea>
+                    </div>
+                    <div className={styles.btns}>
+                        <Button className={styles.buttonCancel} onClick={handlePopup}>Cancel</Button>
+                        <Button className={styles.buttonRevert} onClick={handlePopup}>Revert</Button>
+                    </div>
+                </div>
+            )}
+        </>
     );
 };
